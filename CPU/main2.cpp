5e38c9ca4ae592c61b2acc9aadf5b4743f706ec8
yangchen1314 <chenyang@nudt.edu.cn>
2021-11-29 20:55:05 +0800

    Edges can be counted

--- CPU/main2.cpp
+++ CPU/main2.cpp
@@ -1,6 +1,7 @@
 #include "BenchMark.h"
 #include "Edge_sketch.h"
 #include "Edge_CU.h"
+#include "CMSketch.h"
 #include<vector>
 #include <string>
 using std::vector;
@@ -35,6 +36,7 @@ vector<vector<int> > readFile(string path, int &N)
 	{
 		graph[a].push_back(b) ;
 		count++ ;
+		cout << a << '\t' << b << endl;
 	}
       //  cout<<a<<endl;
 	if(count != m)
@@ -46,9 +48,25 @@ vector<vector<int> > readFile(string path, int &N)
 }
 
 int main(int argc, char *argv[]) {
-	std::cout << "test00";
-        Edge_Sketch<uint64_t, uint64_t> * node = new Edge_Sketch<uint64_t, uint64_t>(500000);
-	Edge_Sketch<uint64_t, uint64_t> * edges = new Edge_Sketch<uint64_t, uint64_t>(500000);
+
+	//std::cout << "test020";
+	int tt;
+	graph = readFile("test1.txt", tt);
+	//std::cout << tt;
+        //Edge_Sketch<uint64_t, uint64_t> * node = new Edge_Sketch<uint64_t, uint64_t>(500000);
+	//Edge_Sketch<uint64_t, uint64_t> * edges = new Edge_Sketch<uint64_t, uint64_t>(500000);
+	CMSketch<uint64_t, uint64_t> *items = new CMSketch<uint64_t, uint64_t>(500000);
+	CMSketch<uint64_t, uint64_t> *sumA = new CMSketch<uint64_t, uint64_t>(500000);
+	CMSketch<uint64_t, uint64_t> *sumB = new CMSketch<uint64_t, uint64_t>(500000);
+
+        for(int i = 0; i < graph.size(); i++)
+		for(int j =0; j < graph[i].size(); j++)
+			items->Insert(i);
+	                //items->Insert
+        
+	for(int i = 0; i < graph.size(); i++)
+		cout << "Results" << <<items->Query(i) << endl;
+
 	/*
     for(uint32_t i = 1;i < argc;++i) {
         std::cout << argv[i] << std::endl;
